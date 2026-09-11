01e05c1891585258e43ed60d093e212794187b4a
Azlan Ainto <azlanainto97@gmail.com>
2026-09-11 13:36:50 +0200

    feat: Klasse Patient mit Validierung und Konsolenausgabe

--- MedFlow/Patient.cs
+++ MedFlow/Patient.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-
-namespace MedFlow;
+﻿namespace MedFlow;
 
 public class Patient
 {
@@ -10,35 +6,36 @@ public class Patient
     public string Vorname { get; set; }
     public string Nachname { get; set; }
     public string Versichertennummer { get; set; }
-    public DateOnly Geburtsdatum {
+
+    public DateOnly Geburtsdatum
+    {
         get => _geburtsdatum;
         set
         {
             GeburtsdatumValidieren(value);
-          
+            _geburtsdatum = value;
         }
     }
-   
-    public Patient(string vorname, string nachname, DateOnly geburtsdatum, string versichertennummer)
+
+    public Patient(string vorname,string nachname,DateOnly geburtsdatum,string versichertennummer)
     {
-       ArgumentNullException.ThrowIfNull("Der Vorname darf nicht leer sein.", nameof(Vorname));
-       ArgumentNullException.ThrowIfNull("Der Vorname darf nicht leer sein.", nameof(Nachname));
-       ArgumentNullException.ThrowIfNull("Der Vorname darf nicht leer sein.", nameof(Geburtsdatum));
-       ArgumentNullException.ThrowIfNull("Der Vorname darf nicht leer sein.", nameof(Versichertennummer));
-       Vorname = vorname;
-       Nachname = nachname;
-       Geburtsdatum = geburtsdatum;
-       Versichertennummer = versichertennummer;
+        ArgumentException.ThrowIfNullOrWhiteSpace(vorname,"Der Vorname darf nicht leer sein.");
+        ArgumentException.ThrowIfNullOrWhiteSpace(nachname,"Der Nachname darf nicht leer sein.");
+        ArgumentException.ThrowIfNullOrWhiteSpace(versichertennummer,"Die Versichertennummer darf nicht leer sein.");
+
+        Vorname = vorname;
+        Nachname = nachname;
+        Versichertennummer = versichertennummer;
+        Geburtsdatum = geburtsdatum;
     }
-    
+
     private static void GeburtsdatumValidieren(DateOnly geburtsdatum)
     {
-        // wenn das Geburtsdatum in der Zukunft(morgen) liegt, dann 
-        // ArgumentOutOfRangeException auswerfen 
         DateOnly heute = DateOnly.FromDateTime(DateTime.Today);
-        if(geburtsdatum > heute)
+
+        if (geburtsdatum > heute)
         {
-            throw new ArgumentOutOfRangeException(nameof(Geburtsdatum), "Das Geburtsdatum darf nicht in der Zukunft liegen.");
+            throw new ArgumentOutOfRangeException(nameof(geburtsdatum),geburtsdatum,"Das Geburtsdatum darf nicht in der Zukunft liegen.");
         }
     }
 
--- MedFlow/Program.cs
+++ MedFlow/Program.cs
@@ -1,9 +1,16 @@
 ﻿namespace MedFlow;
+
 public class Program
 {
     static void Main(string[] args)
     {
-        Patient patient = new("Max","Mustermann", new DateOnly(1985,3,14),"A123456789");
+        Patient patient = new("Max", "Mustermann", new DateOnly(1985, 3, 14), "A123456789");
         Console.WriteLine(patient);
+
+        // Handtest
+
+       //var a = new Patient("Max", "Mustermann", new DateOnly(2030, 1, 1), "A1");   // Zukunft
+      // var b = new Patient("", "Mustermann", new DateOnly(1985, 3, 14), "A1");     // leerer Vorname
+       // var c = new Patient("Max", "Mustermann", new DateOnly(1985, 3, 14), "  ");  // nur Leerzeichen
     }
 }
