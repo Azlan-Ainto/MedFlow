a7ed5b3e73f86aa9f49780371ac100357bead43b
Azlan Ainto <azlanainto97@gmail.com>
2026-09-13 00:43:18 +0200

    feat: PatientenverwaltungTests implementiert.

--- MedFlow.Tests/PatientenverwaltungTests.cs
+++ MedFlow.Tests/PatientenverwaltungTests.cs
@@ -37,19 +37,8 @@ namespace MedFlow.Tests
         public void Anlegen_MitVersichertennummerInAndererSchreibweise_WirftInvalidOperationException()
         {
             var verwaltung = new Patientenverwaltung();
-
-            var ersterPatient = new Patient(
-                "Max",
-                "Min",
-                new DateOnly(1985, 3, 14),
-                "A1234");
-
-            var zweiterPatient = new Patient(
-                "Peter",
-                "Mustermann",
-                new DateOnly(1990, 7, 20),
-                "a1234");
-
+            var ersterPatient = new Patient("Max", "Min", new DateOnly(1985, 3, 14), "A1234");
+            var zweiterPatient = new Patient("Peter", "Mustermann", new DateOnly(1990, 7, 20), "a1234");
             verwaltung.Anlegen(ersterPatient);
             Assert.Throws<InvalidOperationException>(() => verwaltung.Anlegen(zweiterPatient));
             Assert.Single(verwaltung.AlleAbrufen());
