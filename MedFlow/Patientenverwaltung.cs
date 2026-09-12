c60fcd39ed82be743949e1830569f9947b950cd5
Azlan Ainto <azlanainto97@gmail.com>
2026-09-13 00:49:17 +0200

    feat & Init: ........

--- MedFlow/Patientenverwaltung.cs
+++ MedFlow/Patientenverwaltung.cs
@@ -2,13 +2,23 @@
 
 public class Patientenverwaltung
 {
-    private readonly List<Patient> _patienten = new List<Patient>();
+    private readonly List<Patient> _patienten = new();
     public void Anlegen(Patient patient)
     {
-
         ArgumentException.ThrowIfNullOrEmpty(nameof(patient));
+        
+        bool patientExistiert = _patienten.Any(p => string.Equals(
+            p.Versichertennummer, 
+            patient.Versichertennummer, 
+            StringComparison.OrdinalIgnoreCase)
+        );
+        if (patientExistiert)
+        {
+            throw new InvalidOperationException("Ein Patient mit der dieser Versichertennummer existiert bereits.");
+        }
         _patienten.Add(patient);
     }
+
     public IReadOnlyList<Patient> AlleAbrufen()
     {
         return _patienten;
