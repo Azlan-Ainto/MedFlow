0a565aed08d25be8729d0dada3deb5167eb56e15
Azlan Ainto <161608940+Azlan-Ainto@users.noreply.github.com>
2026-09-13 00:54:30 +0200

    Refactor patient existence check in Anlegen method

--- MedFlow/Patientenverwaltung.cs
+++ MedFlow/Patientenverwaltung.cs
@@ -5,13 +5,8 @@ public class Patientenverwaltung
     private readonly List<Patient> _patienten = new();
     public void Anlegen(Patient patient)
     {
-        ArgumentException.ThrowIfNullOrEmpty(nameof(patient));
-        
-        bool patientExistiert = _patienten.Any(p => string.Equals(
-            p.Versichertennummer, 
-            patient.Versichertennummer, 
-            StringComparison.OrdinalIgnoreCase)
-        );
+        ArgumentException.ThrowIfNullOrEmpty(nameof(patient));        
+        bool patientExistiert = _patienten.Any(p => string.Equals(p.Versichertennummer,patient.Versichertennummer, StringComparison.OrdinalIgnoreCase));
         if (patientExistiert)
         {
             throw new InvalidOperationException("Ein Patient mit der dieser Versichertennummer existiert bereits.");
