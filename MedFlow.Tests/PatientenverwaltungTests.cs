7d9fc25a965100656a4d03541a1c84b48f8fd733
Azlan Ainto <azlanainto97@gmail.com>
2026-09-13 21:26:10 +0200

    merge: auf den gleichen Stand bringen

--- MedFlow.Tests/PatientenverwaltungTests.cs
+++ MedFlow.Tests/PatientenverwaltungTests.cs
@@ -14,13 +14,9 @@ public class PatientenverwaltungTests
     [Fact]
     public void Anlegen_MitNeuerVersichertennummer_FuegtPatientHinzu()
     {
-        // Arrange
         var patientenverwaltung = new Patientenverwaltung();
         var patient = new Patient("Max", "Min", new DateOnly(2004, 01, 01), "A123");
-
-        // Act
         patientenverwaltung.TryAnlegen(patient);
-        //Assert
         var patientenListe = patientenverwaltung.AlleAbrufen();
         Assert.Single(patientenListe);
         Assert.Same(patient, patientenListe.First());
